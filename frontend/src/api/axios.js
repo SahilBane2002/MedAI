202eaff60623a41045af92885867e5c9d2897304
sahilbane <sahilbane02@gmail.com>
2025-05-18 20:31:48 -0400

    Khatam

--- frontend/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000',  // backend server address
-  withCredentials: true, // if you're dealing with sessions/cookies
-});
-
-export default api;
